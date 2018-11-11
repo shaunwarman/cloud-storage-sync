ddc216885e01f9834d061bbf9dc468fe58f01ac2
Shaun Warman <swarman@paypal.com>
2018-11-11 16:40:36 -0600

    fix: name reference for src and dest files

--- index.js
+++ index.js
@@ -15,17 +15,17 @@ const copyFile = options => {
 
   const {
     srcBucketName,
-    srcFilename,
+    srcFileName,
     destBucketName,
-    destFilename,
+    destFileName,
     retries = 1
   } = options;
 
   const copy = () => {
     return storage
       .bucket(srcBucketName)
-      .file(srcFilename)
-      .copy(storage.bucket(destBucketName).file(destFilename));
+      .file(srcFileName)
+      .copy(storage.bucket(destBucketName).file(destFileName));
   };
 
   return pRetry(copy, {
@@ -33,7 +33,7 @@ const copyFile = options => {
     onFailedAttempt: error => {
       if (error.attemptsLeft === 0) {
         throw new Error(
-          `Retry count exhausted copying: ${srcBucketName}:${srcFilename} to ${destBucketName}:${destFilename}`
+          `Retry count exhausted copying: ${srcBucketName}:${srcFileName} to ${destBucketName}:${destFileName}`
         );
       }
     }
